bb151c6cfcd5c4e5bfbf1779c5b3000b49fd53b7
AleEirea97 <63886280+AleEirea97@users.noreply.github.com>
2020-10-10 00:52:47 -0300

    Se realiza el ejercicio 35.

---
+++ Código/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char nombre[15];
+	char apellido[15];
+	int edad;
+	long tel;
+	char mail[30];
+}datos_t;
+
+typedef struct {
+    datos_t info;
+    void *lazo;
+} pila_t;
+
+void menu (void){
+	system("cls");
+	printf("**MENU DE CONTROL**");
+	printf("\n1. Cargar Datos.");		//Carga en pila.
+	printf("\n2. Cargar Datos en Archivo.");
+	printf("\n3. Mostrar Archivo.");	//Contactos.dat
+	printf("\n4. SALIR.");
+	printf("\nOPCION:");
+}
+
+int opc(void){
+	int op;
+	scanf("%d", &op);
+	return op;
+}
+
+int main (void){
+
+	FILE *file_p;
+	pila_t *p,*aux;
+	p = aux = NULL;
+	datos_t info_A;
+
+	while(1){
+
+		menu();
+
+		switch(opc()){
+
+			case 1:
+				fflush(stdin);
+				system("cls");
+
+				aux = (pila_t *)malloc(sizeof(pila_t));
+
+				printf("Ingrese su apellido:");
+				gets(aux->info.apellido);
+
+				printf("Ingrese su nombre:");
+				gets(aux->info.nombre);
+
+				fflush(stdin);
+
+				printf("Ingrese su edad:");
+				scanf("%d", &aux->info.edad);
+
+				printf("Ingrese su telefono:");
+				scanf("%ld", &aux->info.tel);
+
+				fflush(stdin);
+
+				printf("Ingrese su direccion de e-mail:");
+				gets(aux->info.mail);
+
+				aux->lazo = p;
+
+				p = aux;
+
+				system("pause");
+			break;
+
+			case 2:
+				system("cls");
+
+				file_p = fopen("contactos.dat","ab");
+
+				aux = p;
+
+				while(1){
+					if(!aux)
+						break;
+
+					if (aux->info.edad > 21)
+						fwrite(&aux->info,sizeof(datos_t),1,file_p);
+
+					aux = (pila_t *)aux->lazo;
+				}
+				fclose(file_p);
+
+				system("pause");
+			break;
+
+			case 3:
+				system("cls");
+
+				file_p = fopen("contactos.dat","rb");
+
+				fread(&info_A,sizeof(datos_t),1,file_p);
+
+				while(!feof(file_p)){
+					printf("\nApellido: %s",info_A.apellido);
+					printf("\nNombre: %s", info_A.nombre);
+					printf("\nEdad: %d", info_A.edad);
+					printf("\nTelefono: %ld", info_A.tel);
+					printf("\nE-Mail: %s", info_A.mail);
+					fread(&info_A,sizeof(datos_t),1,file_p);
+
+					printf("\n\n");
+				}
+				fclose(file_p);
+
+				system("pause");
+			break;
+
+			case 4:
+				return 0;
+		}
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TPII-Estructuras-Autoreferenciadas.
+* EJERCICIO 35:
+
+<p align ="justify">
+Hacer un programa que tome los datos de contacto de una persona (Nombre, Apellido, edad, teléfono, mail) y los cargue, en forma directa, en una pila de memoria dinámica. Imprimir en pantalla y cargar en un archivo llamado "contactos.dat", de organización secuencial, los registros ingresados por el usuario si la persona tiene una edad mayor a 21 años.
+</p>
