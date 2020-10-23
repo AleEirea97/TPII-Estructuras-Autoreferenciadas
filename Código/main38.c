6eeb688eb3bf612b9dc40880669e08e4175ff191
AleEirea97 <63886280+AleEirea97@users.noreply.github.com>
2020-10-23 14:18:11 -0300

    Se modifica el main38.c

--- Código/main38.c
+++ Código/main38.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define REGBIT(byte,bit) ((volatile fbit_t*)&byte)->b##bit
-
-typedef struct{
-	unsigned char b0:1;
-	unsigned char b1:1;
-	unsigned char b2:1;
-	unsigned char b3:1;
-	unsigned char b4:1;
-	unsigned char b5:1;
-	unsigned char b6:1;
-	unsigned char b7:1;
-}fbit_t;
-
 typedef struct{
     int muestra;
     void *lazo;
@@ -34,55 +21,6 @@ int opc(void){
 	return op;
 }
 
-fbit_t upload(fbit_t a){
-	int b;
-	for(int i = 0; i < 8; i++){
-		switch(i){
-			case 0:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,7) = b;
-			break;
-			case 1:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,6) = b;
-			break;
-			case 2:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,5) = b;
-			break;
-			case 3:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,4) = b;
-			break;
-			case 4:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,3) = b;
-			break;
-			case 5:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,2) = b;
-			break;
-			case 6:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,1) = b;
-			break;
-			case 7:
-				printf("Sensando...");
-				scanf("%d",&b);
-				REGBIT(a,0) = b;
-			break;
-		}
-	}
-	return a;
-}
-
 int main (void){
 
 	cola_t *p = NULL, *u = NULL, *aux;
@@ -92,27 +30,33 @@ int main (void){
 		switch(opc()){
 
 			case 1:
-
-				aux = (cola_t *)malloc(sizeof(cola_t));
-				if(aux)					//Si aux no es NULL
-				{
-					printf("\n Muestra:\n");
-					aux->muestra = upload(aux->muestra);
-
-					if(p == NULL)		//Tratamiento especial al ser el primer elemento.
-						p=u=aux;
-					else
+			
+					for(int i = 0; i < 8; i ++)
 					{
-						u->lazo =(void *)aux;	//Engancha el último con el recién creado.
-						u = aux;					//Actualiza  el último.
-					}
-					u->lazo = NULL;		//Final de cola.
-				}
-				else
-					printf("\nMemoria Insuficiente.");
+						printf("\n Muestreando...");
+						system("cls");
 
-				system("pause");
-				break;
+						aux = (cola_t *)malloc(sizeof(cola_t));
+
+						if(aux)					//Si aux no es NULL
+						{
+							aux->muestra = muestra[i];
+
+							if(p == NULL)					//Tratamiento especial al ser el primer elemento.
+								p=u=aux;
+								else
+								{
+									u->lazo =(void *)aux;		//Engancha el último con el recién creado.
+									u = aux;					//Actualiza  el último.
+								}
+								u->lazo = NULL;					//Final de cola.
+							}
+							else
+									printf("\nMemoria Insuficiente.");
+						}
+
+						system("pause");
+						break;
 
 			case 2:
 
